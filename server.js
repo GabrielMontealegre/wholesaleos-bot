69a482e2d8260a0fdc80ec15cec1f5d2a319b560
GabrielMontealegre <gabrielmontealegre2@gmail.com>
2026-05-05 17:08:16 -0700

    deploy: courthouse fully wired, wos-features v9 courthouse button

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Deploy: 2026-05-06T00:02:59.049Z
+// Deploy: 2026-05-06T00:08:13.221Z
 // server.js Ã¢ÂÂ Express server for dashboard + REST API
 // Serves dashboard at /dashboard/ and API at /api/
 
